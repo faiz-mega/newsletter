885a5660d7a9fc6576bfbc6e751ad857b7d1625f
Suresh <skmud721@gmail.com>
2023-05-03 14:40:25 +0530

    commit-m4

--- app.js
+++ app.js
@@ -2,9 +2,26 @@ const express = require("express");
 const request = require("request");
 const bodyParser = require("body-parser");
 const https = require("https");
+const { initializeApp } =  require("firebase/app");
+const { getAnalytics } =  require("firebase/analytics");
 
 const app = express();
 const port = 3000;
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDq6WL-KC6stXE1R2v-6c_k-0VVndOzquA",
+    authDomain: "newsletter-83467.firebaseapp.com",
+    projectId: "newsletter-83467",
+    storageBucket: "newsletter-83467.appspot.com",
+    messagingSenderId: "76838419442",
+    appId: "1:76838419442:web:2485def2b4766a003b896b",
+    measurementId: "G-Z9CQS9XJR8"
+  };
+  
+// Initialize Firebase
+const myApp = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
@@ -33,7 +50,6 @@ app.post("/", function(req,res){
         merge_fields: {
             FNAME: firstName,
             LNAME: lastName,
-            EMAIL: email,
         },
     };
     const dataJson = JSON.stringify(data);
@@ -57,6 +73,6 @@ app.post("/failure", function(req,res){
     res.redirect("/");
 });
 
-app.listen(port, function(){
+app.listen(port || process.env.PORT, function(){
     console.log("Server is running on port " + port);
 });
